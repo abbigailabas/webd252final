bf0d5d06a28eb11d4e59e15c307b5d66bc0877a8
Abby <abbigail.abas@gmail.com>
2016-04-12 17:22:42 -0400

    trying to keep up

--- admin/login-action.php
+++ admin/login-action.php
@@ -1,5 +1,46 @@
 <?php 
 
+include ('../database.php');
+
+
+//load user with matching username
+$statement = $db_connection->prepare(
+	"SELECT * FROM User WHERE Username = ?"
+);
+
+// Replace ? with username
+$statement->bind_param(
+	's',
+	$_POST['Username']
+);
+
+// Run SQL query
+$statement->execute();
+
+// Get result
+$result = $statement->get_result();
+
+// user found
+if ($result) {
+	$user = $result->fetch_assoc();
+
+	// chech if password is correct
+	/if ($_POST['Password'] == $user['Password']) {
+
+		// Set flag in session
+		session_start();
+		$_SESSION['flag'] = TRUE;
+
+		// Redirect
+		header('location:')
+	}
+
+
+}
+
+
+
+
 
 // check if username and password are correct
 if ($_POST['Username'] == 'admin' && $_POST['Password'] == 'password') {
